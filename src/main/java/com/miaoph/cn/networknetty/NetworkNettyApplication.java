be71ede9662dd634154ca8303af8e6450ecdbfca
miaopenghang <miaopenghang@miaopenghangdeMacBook-Pro.local>
2020-04-09 15:00:52 +0800

    init

---
+++ src/main/java/com/miaoph/cn/networknetty/NetworkNettyApplication.java
@@ -0,0 +1,13 @@
+package com.miaoph.cn.networknetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkNettyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miaoph/cn/networknetty/NetworkNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoph.cn.networknetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
